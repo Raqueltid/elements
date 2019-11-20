98eb31ea0e30cd2abee1c3e1c6d1396065fc902a
rflores_atsistemas <rflores@atsistemas.com>
2019-11-20 15:24:25 +0100

    example with 2 web-components

---
+++ elements-build.js
@@ -0,0 +1,25 @@
+const fs = require("fs-extra");
+const concat = require("concat");
+
+(async function build() {
+  const files = [
+    "./dist/elements/runtime-es2015.js",
+    "./dist/elements/polyfills-es2015.js",
+    "./dist/elements/scripts.js",
+    "./dist/elements/main-es2015.js"
+  ];
+
+  //Generar JS
+  await fs.ensureDir("elements");
+  await concat(files, "elements/elements.js");
+
+  //Generar CSS
+  await fs.ensureDir("elements/assets/elements");
+  await fs.copyFile(
+    "./dist/elements/styles.css",
+    "elements/assets/elements/styles.css"
+  );
+
+  // Generar ASSETS
+  await fs.copy("src/assets", "elements/assets");
+})();
